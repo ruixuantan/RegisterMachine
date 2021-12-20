685049adcd3f5affdaa1d834fad81a497d187574
ruixuantan <ruixuantan@yahoo.com.sg>
2021-12-20 17:09:16 +0800

    Initial commit

---
+++ build/CMakeFiles/RegisterMachine.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/RegisterMachine.dir/src/RegisterMachine.cpp.o"
+  "CMakeFiles/RegisterMachine.dir/src/RegisterMachine.cpp.o.d"
+  "RegisterMachine"
+  "RegisterMachine.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/RegisterMachine.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
